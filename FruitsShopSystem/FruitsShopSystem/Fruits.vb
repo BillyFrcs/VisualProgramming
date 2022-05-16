ecef2d0e442e1bd1da1f26a55731059e7248fd86
Billy Franscois <BillyFranscois@outlook.com>
2022-05-16 18:04:40 +0900

    Added new Assets

---
+++ FruitsShopSystem/FruitsShopSystem/Fruits.vb
@@ -0,0 +1,10 @@
+﻿Namespace Shop
+    Public Class Fruits
+        Public Shared Property Apple As Integer = 10000
+        Public Shared Property Grapes As Integer = 25000
+        Public Shared Property Lemon As Integer = 12000
+        Public Shared Property Pear As Integer = 30000
+        Public Shared Property Pineapple As Integer = 18000
+        Public Shared Property Strawberry As Integer = 40000
+    End Class
+End Namespace
