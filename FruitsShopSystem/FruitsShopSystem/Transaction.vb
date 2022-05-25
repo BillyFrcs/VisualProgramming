2cc65bab7f5f4f0b9f88e9fead7a2c42ca7261ee
Billy Franscois <BillyFranscois@outlook.com>
2022-05-25 10:41:59 +0900

    Update Target Framework

--- FruitsShopSystem/FruitsShopSystem/Transaction.vb
+++ FruitsShopSystem/FruitsShopSystem/Transaction.vb
@@ -18,9 +18,6 @@ Public Class Transaction
     Private _timeTransaction As String
     Private _dateTransaction As String
 
-    ' Email validation
-    Private ReadOnly _regex As New Regex("^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
-
     ' SQL Connection to the database
     Private Const _serverName = "BILLY"
     Private Const _databaseName = "FruitsShop"
